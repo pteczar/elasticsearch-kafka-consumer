68d8093de907b7688af249ba91749bb533b89f87
pteczar <p.teczar@gmail.com>
2020-03-31 16:58:50 +0200

    implemented sending the data from producer into elastic search

--- src/main/java/com/github/pteczar/kafka/elasticsearch_consumer/ElasticSearchConsumer.java
+++ src/main/java/com/github/pteczar/kafka/elasticsearch_consumer/ElasticSearchConsumer.java
@@ -1,11 +1,17 @@
 package com.github.pteczar.kafka.elasticsearch_consumer;
 
+
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -17,6 +23,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
 
@@ -50,23 +60,62 @@ public class ElasticSearchConsumer {
         return client;
     }
 
+    public static KafkaConsumer<String, String> createConsumer(String topic) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-elasticsearch"; // start three consumers within this group and it will balance and redolence itself
+        //String topic = "twitter_tweets";
+
+
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // earliest - reads from the beginning or latest since the last change
+
+        //create consumer
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+
+    }
+
     public static void main(String[] args) throws IOException {
 
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
         RestHighLevelClient client = createClient();
 
-        String jsonString = "{ \"du\": \"pa\" }";
 
-        IndexRequest indexRequest = new IndexRequest(
-                "twitter"
-        ).source(jsonString, XContentType.JSON);
 
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        logger.info(id);
+        KafkaConsumer<String,String> consumer = createConsumer("twitter_tweets");
+
+        while(true) {
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); //duration
+
+            for (ConsumerRecord<String, String> record : records) {
+                //inserting data into elastic search
+              String jsonString =  record.value(); // or tweet
+
+                IndexRequest indexRequest = new IndexRequest(
+                        "twitter"
+                ).source(jsonString, XContentType.JSON);
+
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                String id = indexResponse.getId();
+                logger.info(id);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         //closing the client
-        client.close();
+        //client.close();
 
     }
 }
